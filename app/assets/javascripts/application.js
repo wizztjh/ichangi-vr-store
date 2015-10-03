2298a448e30ee4a2dcd4db232b830626b63056b1
Tee Jia Hen <wizztjh@gmail.com>
2015-10-03 21:08:54 +0800

    merge with indexhtml

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,32 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  var changiLogo = $('#changi-logo');
+  var video = $('#video-on-left-wall');
+  var tableOnWall = document.getElementsByClassName('table-on-wall');
+  var item = $('#item-on-wall-table');
+  var metaVerse = $('#meta-verse');
+  var scene = metaVerse[0].controller.gameObject.scene;
+  changiLogo[0].controller.metaObject.group.position.z = changiLogo[0].controller.properties.length / 2;
+  video[0].controller.metaObject.group.position.z = 2;
+  for (var i = 0; i < tableOnWall.length; i++) {
+    tableOnWall[i].controller.metaObject.group.position.z = 2
+  }
+  item[0].controller.metaObject.group.position.z = 4;
+  var urlPrefix = "assets/img/betterskybox_";
+   var directions  = [ "zneg", "zpos", "ypos", "yneg", "xpos", "xneg"];
+  var imageSuffix = ".png";
+  var skyGeometry = new THREE.BoxGeometry( 5000, 5000, 5000 );
+  var materialArray = [];
+  for (var i = 0; i < 6; i++){
+    materialArray.push( new THREE.MeshBasicMaterial({
+      map: THREE.ImageUtils.loadTexture( urlPrefix + directions[i] + imageSuffix ),
+      side: THREE.BackSide
+    }));
+  }
+  var skyMaterial = new THREE.MeshFaceMaterial( materialArray );
+  var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
+  scene.add( skyBox );
+});
